c4d731d6fd1bf791329693131d7787916c6f306c
Сергей <!@>
2020-09-21 11:42:13 +0300

    add service

--- app/src/main/java/com/example/widjet/main/MyWidget.java
+++ app/src/main/java/com/example/widjet/main/MyWidget.java
@@ -1,9 +1,17 @@
 package com.example.widjet.main;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
 import android.widget.RemoteViews;
+
+import androidx.annotation.Nullable;
 
 import com.example.widjet.R;
 import com.example.widjet.main.database.App;
@@ -23,15 +31,22 @@ import java.util.List;
 public class MyWidget extends AppWidgetProvider {
 
     private Date time;
+    private final String UPDATE_WIDGET = "updateWidget";
+
+    private PendingIntent createUpdatePendIntent(Context context) {
+        Intent intent = new Intent(UPDATE_WIDGET);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return pendingIntent;
+    }
 
     //onUpdate вызывается при обновлении виджета. На вход, кроме контекста, метод получает объект AppWidgetManager и список ID экземпляров виджетов, которые обновляются. Именно этот метод обычно содержит код, который обновляет содержимое виджета. Для этого нам нужен будет AppWidgetManager, который мы получаем на вход.
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        for (int appWidgetId : appWidgetIds) {
+        context.startService(new Intent(context, UpdateService.class));
+ /*       for (int appWidgetId : appWidgetIds) {
             updateTimeWidget(context, appWidgetManager, appWidgetId);
-        }
+        }*/
     }
 
     //onDeleted вызывается при удалении каждого экземпляра виджета. На вход, кроме контекста, метод получает список ID экземпляров виджетов, которые удаляются.
@@ -46,12 +61,92 @@ public class MyWidget extends AppWidgetProvider {
         super.onEnabled(context);
         //TODO хрен знает здесь нужно начальную базу создать или нет???
         System.out.println("enabled widget");
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 1);
+
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 1000, createUpdatePendIntent(context));
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (UPDATE_WIDGET.equals(intent.getAction())) {
+            context.startService(new Intent(context, UpdateService.class));
+        }
+    }
+
+
+    public static class UpdateService extends Service {
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onStart(Intent intent, int startId) {
+            super.onStart(intent, startId);
+            RemoteViews views = updateTimeWidget(this);
+            ComponentName widget = new ComponentName(this, MyWidget.class);
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+            appWidgetManager.updateAppWidget(widget, views);
+        }
+
+        private RemoteViews updateTimeWidget(Context context) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar calendar = new GregorianCalendar();
+            calendar.setTime(new Date());
+
+            views.setTextViewText(R.id.timeImg,
+                    calendar.get(Calendar.HOUR_OF_DAY) + " : " + (calendar.get(Calendar.MINUTE) < 10 ? "0 " + calendar.get(Calendar.MINUTE) : calendar.get(Calendar.MINUTE)));
+            views.setTextViewText(R.id.dateImg, simpleDateFormat.format(calendar.getTime()));
+
+            return views;
+
+
+       /* calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0); // facking calendar need set millsecond every time
+        // TODO for testing in code удалить-----
+        //    calendar.set(Calendar.YEAR, 2023);
+        // TODO --------------
+        Date time = calendar.getTime();
+
+        System.out.println(simpleDateFormat.format(time));
+        System.out.println(time.getTime());
+
+        PrazdnikDTO prazdnik = getPrazdnik(time);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+        views.setTextViewText(R.id.timeImg, getTime());
+        views.setTextViewText(R.id.dateImg, simpleDateFormat.format(time));
+        views.setTextViewText(R.id.textImg, prazdnik.getName());
+        System.out.println(prazdnik.getImg());
+
+        views.setImageViewResource(R.id.img, context.getResources().getIdentifier("drawable/" + prazdnik.getImg(),
+                null,
+                context.getPackageName()));
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);*/
+        }
+    }
+
 
     //onDisabled вызывается при удалении последнего экземпляра виджета.
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
+        // как я понимаю удаляет интент с которым в сервисе что то крутится
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(createUpdatePendIntent(context));
     }
 
     private void updateTimeWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
@@ -62,7 +157,7 @@ public class MyWidget extends AppWidgetProvider {
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0); // facking calendar need set millsecond every time
         // TODO for testing in code удалить-----
-    //    calendar.set(Calendar.YEAR, 2023);
+        //    calendar.set(Calendar.YEAR, 2023);
         // TODO --------------
         time = calendar.getTime();
 
